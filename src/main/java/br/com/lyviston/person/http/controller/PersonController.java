7ebd401ce44d074cc32150a96eca79db92be5e7a
Lyviston da Silva <lyviston@live.com>
2022-05-28 21:59:56 -0300

    adicionado openApi 3

--- src/main/java/br/com/lyviston/person/http/controller/PersonController.java
+++ src/main/java/br/com/lyviston/person/http/controller/PersonController.java
@@ -1,4 +1,24 @@
 package br.com.lyviston.person.http.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
 public class PersonController {
+
+    @GetMapping
+    @Operation(summary = "Retorna um oi")
+    public ResponseEntity<String> oi(){
+        return ResponseEntity.ok("oi");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> ola(){
+        return ResponseEntity.ok("ola");
+    }
 }
